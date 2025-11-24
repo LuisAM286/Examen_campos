42cb241f1da7952253c05e10f9955f22e8b2b401
Luissss <luisangelalatorremorales99@gmail.com>
2025-11-24 13:37:57 -0600

    Add files via upload

--- d02-p10-equipo-04.c
+++ d02-p10-equipo-04.c
@@ -2,10 +2,30 @@
 #include <time.h> //Garantiza diferentes secuencias de numeros aleatorios
 #include <stdlib.h>// Agrega la funcion rand() para generar numeros aleatorios
 #include <conio.h>
+#include <string.h>
+#include <windows.h>
 
 
 #define total 30
 #define generar 10
+
+
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void centrarTexto(int fila, const char *texto){
+    int ancho = 80;
+    int largo = strlen(texto);
+    int x =(ancho-largo) / 2;
+
+    gotoxy(x, fila);
+    printf("%s",texto);
+}
 
 //Funcion para evitar que se repitan preguntas en una sesión
 void barajar(int *array, int n){ //
@@ -17,30 +37,90 @@ void barajar(int *array, int n){ //
 
     }
 }
+
+void evaluarRespuesta(char res, int indice){
+    static char *respuestas[]={
+        "A","B","B","A","C","C","A","B","A","C",
+        "B","A","C","C","A","A","B","C","A","C",
+        "B","A","A","B","A","B","B","C","C","C"
+    };
+    if(res == respuestas[indice][0]){
+            gotoxy(35,11);
+            printf("respuesta correcta");
+    }else{
+        gotoxy(35,11);
+        printf("respuesta incorrecta");
+    }
+}
+
+void presentarPreguntas(int indice){
+    static char *preguntas[] = { //se usa static para no borrar el arreglo
+        " Pregunta 1"," Pregunta 2"," Pregunta 3"," Pregunta 4"," Pregunta 5"," Pregunta 6",
+        " Pregunta 7"," Pregunta 8"," Pregunta 9"," Pregunta 10"," Pregunta 11"," Pregunta 12",
+        " Pregunta 13"," Pregunta 14"," Pregunta 15"," Pregunta 16"," Pregunta 17"," Pregunta 18",
+        " Pregunta 19"," Pregunta 20"," Pregunta 21"," Pregunta 22"," Pregunta 23"," Pregunta 24",
+        " Pregunta 25"," Pregunta 26"," Pregunta 27"," Pregunta 28"," Pregunta 29"," Pregunta 30",
+    };
+    static char *opciones[] = {
+        "A)",
+        "B)",
+        "C)"
+
+    };
+    centrarTexto(4, preguntas[indice]);
+    centrarTexto(5, opciones[0]);
+    centrarTexto(6, opciones[1]);
+    centrarTexto(7, opciones[2]);
+
+}
+
 //Función principal
 int main(){
     srand(time(NULL));//Inicializa el generador de numeros aleatorios
 
     int numeros[total];
     char nombre[50];
-    for(int i = 0;i < 3;i++){
+    for(int cont = 0;cont < 3;cont++){
         system("cls");
 
+        gotoxy(35,6);
         printf("Ingresa tu nombre: ");
         fgets(nombre,50, stdin);
 
         for(int i = 0; i<total; i++){
-        numeros[i] = i +1;
+            numeros[i] = i +1;
         }
 
-        barajar(numeros, total);
+        barajar(numeros,total);
 
-        printf("10 numeros aleatorios unicos generados: \n\n");
+        gotoxy(35, 3);
+        printf("Preguntas:");
         for(int i = 0; i<generar; i++){
-            printf("%d\n", numeros[i]);
+            char res;
+            system("cls");
+            int indice = numeros[i]-1;
+            int progreso = 1;
+            for(;;){
+                system("cls");
+                gotoxy(50,3);
+                printf("%i / 10",progreso);
+                presentarPreguntas(indice);
+                gotoxy(35,9);
+                scanf(" %c", &res);
+                if(res != 'A' && res != 'B' && res!= 'C'){//Comillas simples para caracteres
+                    gotoxy(35,10);
+                    printf("Opcion no valida, ingresa A,B o C");
+                    getch();
+                }else{
+                    evaluarRespuesta(res, indice);
+                    break;
+                }
+            }
+            getch();
+            system("cls");
         }
-        printf("\n");
-        printf("%s\nPresiona cualquier tecla para continuar.", nombre);
+        gotoxy(35, 15);
+        printf("%s, Presiona cualquier tecla para continuar.", nombre);
         getch();
 
     }
